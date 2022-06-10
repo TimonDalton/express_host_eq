c109dec32256a2a85da67761dd23f3ca81b79a5d
TimonDalton <timondalton2001@gmail.com>
2022-06-11 00:58:13 +0200

    updated port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
  
 app.use(express.static(path.resolve('../Equationator', 'public')));
  
-const PORT = 420;
+const PORT = 80;
 app.listen(PORT, () => console.log('listening on port', PORT));
